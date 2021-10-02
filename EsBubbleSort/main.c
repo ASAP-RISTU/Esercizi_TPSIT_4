04044288ed0bab59907222346e0304fce79aa7ec
Davide Ristorto <91005671+ASAP-RISTU@users.noreply.github.com>
2021-10-02 10:34:33 +0200

    Add files via upload

---
+++ EsBubbleSort/main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define DIM 5
+
+void ordinaVettore(int vett[], int n) {
+    int sup, i, a;
+
+    for(sup = n - 1; sup > 0; sup--) {
+        for(i = 0; i < sup; i++) {
+            if(*(vett+i+1) < *(vett+i)) {
+                a = *(vett+i+1);
+                *(vett+i+1) = *(vett+i);
+               *(vett+i) = a;
+            }
+        }
+    }
+}
+
+void caricaVettore(int vett[], int n) {
+    int k=0;
+
+    for(k=0; k<n; k++) {
+        printf("inserisci numero: ");
+        scanf("%d",&*(vett+k));
+    }
+}
+
+void stampa(int vett[], int n) {
+    int k=0;
+
+    for(k=0; k<n; k++) {
+        printf("%d  ",*(vett+k));
+    }
+}
+
+int main() {
+    int vett[DIM];
+
+    caricaVettore(vett,DIM);
+    ordinaVettore(vett,DIM);
+    stampa(vett,DIM);
+    return 0;
+}
