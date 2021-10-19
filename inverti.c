bcdca6a69276d688c18b8a1bc86e8bdfe9997976
Davide Ristorto <91005671+DavideRistorto@users.noreply.github.com>
2021-10-19 16:03:17 +0200

    Add files via upload

---
+++ inverti.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 5
+
+void inverti(int v[],int dim) {
+    int k=0;    //contatore
+    int j=dim-1;    //secondo contatore che parte da min-1(ultima cella del vettore)
+    int temp; //per caricare temporaneamente il valore del vettore
+    for(k=0; k<dim/2; k++) {    //inzio del ciclo
+        temp=v[k];
+        v[k]=v[j];  //mette nella prima cella il valore dell ultima cella
+        v[j]=temp;  //mette nell ultima cella temp che ha il valore della prima
+        j--;
+    }
+}
+
+void stampa(int v[],int dim){  //ciclo for per stampare il vettore
+for(int k=0;k<dim;k++){
+    printf("%d ",v[k]);
+}
+}
+int main() {
+    int v[MAX];
+    for(int k=0; k<MAX; k++) {      //ciclo per caricare il vettore
+        printf("inserisci valore: ");
+        scanf("%d",&v[k]);
+    }
+    inverti(v,MAX);
+    stampa(v,MAX);
+
+    return 0;
+}
