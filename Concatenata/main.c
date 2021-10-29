d0cacf5478664e968727bc708873ca9ab2be0aaa
Davide Ristorto <91005671+DavideRistorto@users.noreply.github.com>
2021-10-29 08:33:55 +0200

    Add files via upload

---
+++ Concatenata/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#define LUNG1 10
+#define LUNG2 50
+
+/*Concatenare due stringhe senza strcat*/
+
+void unisci(char *str1, char *str2)
+{
+    int n1=strlen(str1); //determinare lunghezza stringhe
+    int n2=strlen(str2);
+    for(int k=0; k< n2; k++)
+    {
+        *(str1+n1+k)= *(str2+k);
+    }
+    *(str1+n1+n2)='\0';
+}
+
+int main()
+{
+
+    char * str1= (char *) malloc(LUNG2 * sizeof(char)); //crea vettore caratteri di lunghezza LUNG
+    char * str2= (char *) malloc(LUNG1 * sizeof(char));
+    printf("prima stringa: ");
+    scanf("%s", str1);
+    printf("seconda stringa: ");
+    scanf("%s", str2);
+    unisci(str1, str2); //la stringa str1 viene modificata e unita alla seconda
+    printf("stringa unita: %s", str1);
+
+    return 0;
+}
