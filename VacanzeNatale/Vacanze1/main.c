95a196beec5a955b06e47fcf33f49532a177d9e6
Davide Ristorto <91005671+DavideRistorto@users.noreply.github.com>
2022-02-24 18:32:30 +0100

    Update main.c

--- VacanzeNatale/Vacanze1/main.c
+++ VacanzeNatale/Vacanze1/main.c
@@ -1,4 +1,8 @@
 //es 1 vacanze di natale
+//Considerate una sequenza di interi letti da input e definite una funzione C che listampa
+//in modo tale che tutti i pari precedano i dispari, nello stesso ordine in cui vengono letti
+//Davide Ristorto 27/12/2021
+
 #include <stdio.h>
 #include <stdlib.h>
 #define DIM 10
@@ -7,14 +11,14 @@
 int pila[DIM_MAX];
 int top = -1;
 
-int pilaPiena(){
+int pilaPiena(){ // verifica se la pila è piena o può ricevere altri dati
     if(top == DIM_MAX)
         return 1;
     else
         return 0;
 }
 
-void push(int data){
+void push(int data){ //vengono inseriti i dati
     if(!pilaPiena()){
         top++;
         pila[top] = data;
@@ -23,7 +27,7 @@ void push(int data){
         printf("ERRORE -> pila piena");
 }
 
-void ordinaPila(int vet[]){
+void ordinaPila(int vet[]){ //funzione per ordinare i dati come da consegna
     for(int k=0;k<DIM;k++){
         if(vet[k]%2==0){
             push(vet[k]);
@@ -37,7 +41,7 @@ void ordinaPila(int vet[]){
 
 int main()
 {
-    int vettore[DIM];
+    int vettore[DIM]; //vettore d'appoggio per prendere i dati
     for (int k=0;k<DIM;k++){
         printf("inserisci un valore: ");
         scanf("%d",&vettore[k]);
